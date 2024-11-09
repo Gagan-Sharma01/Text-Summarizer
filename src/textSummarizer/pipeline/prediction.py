5baf33b9dcdccca70e80c53eed7ce38e759f027b
Gagan Sharma <Gagan Sharma>
2024-11-10 01:58:28 +0530

    App formation and testing done

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -4,21 +4,22 @@ from transformers import pipeline
 
 
 class PredictionPipeline:
-    def _init__(self):
+    def __init__(self):
         self.config = ConfigurationManager().get_model_evaluation_config()
-        
-        
+
+
+    
     def predict(self,text):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams":18, "max_length" : 128}
-        
-        pipe = pipeline("Summarization", model=self.config.model_path,tokenizer=tokenizer)
-        
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+
+        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+
         print("Dialogue:")
         print(text)
-        
-        output = pipe(text, **gen_kwargs)[0]["Summary_text"]
+
+        output = pipe(text, **gen_kwargs)[0]["summary_text"]
         print("\nModel Summary:")
         print(output)
-        
+
         return output
